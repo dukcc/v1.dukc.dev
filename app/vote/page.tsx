3143f9f40bb9c5eec4b0dd8b37e78a37bff487ba
dukc <97273940+dukcc@users.noreply.github.com>
2024-01-24 19:10:10 +0000

    removed margin t

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -4,14 +4,14 @@ export default function Vote() {
   return (
     <div className="w-full overflow-y-auto overflow-x-hidden flex flex-col justify-center items-center h-full p-64 gap-24 max-[1000px]:p-24 bg-bg-1 border border-stroke-1 backdrop-blur-main rounded-out">
       <h3 className="text-3 font-medium">Glassmorphic</h3>
-      <div className="grid grid-cols-4 max-[1800px]:grid-cols-3 w-full h-fit mt-[932px] max-[1500px]:grid-cols-2 max-[1200px]:grid-cols-1 gap-24">
+      <div className="grid grid-cols-4 max-[1800px]:grid-cols-3 w-full h-fit max-[1500px]:grid-cols-2 max-[1200px]:grid-cols-1 gap-24">
         <Image src={"/vote/glass/Home.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
         <Image src={"/vote/glass/Short.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
         <Image src={"/vote/glass/Video.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
         <Image src={"/vote/glass/Sign In.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
       </div>
       <h3 className="text-3 font-medium">Neumorphic</h3>
-      <div className="grid grid-cols-4 max-[1800px]:grid-cols-3 w-full h-fit mt-[932px] max-[1500px]:grid-cols-2 max-[1200px]:grid-cols-1 gap-24">
+      <div className="grid grid-cols-4 max-[1800px]:grid-cols-3 w-full h-fit max-[1500px]:grid-cols-2 max-[1200px]:grid-cols-1 gap-24">
         <Image src={"/vote/neu/Home.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
         <Image src={"/vote/neu/Short.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
         <Image src={"/vote/neu/Video.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />

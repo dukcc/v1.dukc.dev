8ba9d35c9cb7c2dcf85f004ff92fc4495e2e538e
dukc <97273940+dukcc@users.noreply.github.com>
2024-01-24 19:17:52 +0000

    aaaaaaaaaaaaaaaaaaaa

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -4,18 +4,18 @@ export default function Vote() {
   return (
     <div className="w-full overflow-y-auto overflow-x-hidden flex flex-col justify-center items-center h-full p-64 gap-24 max-[1000px]:p-24 bg-bg-1 border border-stroke-1 backdrop-blur-main rounded-out">
       <h3 className="text-3 font-medium">Glassmorphic</h3>
-      <div className="grid grid-cols-4 max-[1800px]:grid-cols-3 w-full h-fit max-[1500px]:grid-cols-2 max-[1200px]:grid-cols-1 gap-24">
-        <Image src={"/vote/glass/Home.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
-        <Image src={"/vote/glass/Short.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
-        <Image src={"/vote/glass/Video.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
-        <Image src={"/vote/glass/Sign In.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
+      <div className="grid grid-cols-4 max-[1800px]:grid-cols-3 w-full h-fit mt-[944px] max-[1500px]:grid-cols-2 max-[1500px]:mt-[1888px] max-[1200px]:grid-cols-1 gap-24">
+        <Image src={"/vote/glass/Home.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
+        <Image src={"/vote/glass/Short.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
+        <Image src={"/vote/glass/Video.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
+        <Image src={"/vote/glass/Sign In.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
       </div>
       <h3 className="text-3 font-medium">Neumorphic</h3>
       <div className="grid grid-cols-4 max-[1800px]:grid-cols-3 w-full h-fit mt-[944px] max-[1500px]:grid-cols-2 max-[1500px]:mt-[1888px] max-[1200px]:grid-cols-1 gap-24">
-        <Image src={"/vote/neu/Home.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
-        <Image src={"/vote/neu/Short.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
-        <Image src={"/vote/neu/Video.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
-        <Image src={"/vote/neu/Sign In.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto" />
+        <Image src={"/vote/neu/Home.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
+        <Image src={"/vote/neu/Short.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
+        <Image src={"/vote/neu/Video.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
+        <Image src={"/vote/neu/Sign In.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
       </div>
       <h2 className="text-2 font-medium">Vote Here</h2>
       <iframe

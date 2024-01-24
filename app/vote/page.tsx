639b6eade18dbe0354c6ec5f3516f0ace1bb8c19
dukc <97273940+dukcc@users.noreply.github.com>
2024-01-24 19:22:33 +0000

    MARGIN TOP

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -4,7 +4,7 @@ export default function Vote() {
   return (
     <div className="w-full overflow-y-auto overflow-x-hidden flex flex-col justify-center items-center h-full p-64 gap-24 max-[1000px]:p-24 bg-bg-1 border border-stroke-1 backdrop-blur-main rounded-out">
       <h3 className="text-3 font-medium">Glassmorphic</h3>
-      <div className="grid grid-cols-4 max-[1800px]:grid-cols-3 w-full h-fit mb-[944px] max-[1500px]:grid-cols-2 max-[1500px]:mb-[1888px] max-[1200px]:grid-cols-1 gap-24">
+      <div className="grid grid-cols-4 max-[1800px]:grid-cols-3 w-full h-fit mt-[944px] max-[1500px]:grid-cols-2 max-[1500px]:mt-[1888px] max-[1200px]:grid-cols-1 gap-24">
         <Image src={"/vote/glass/Home.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
         <Image src={"/vote/glass/Short.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
         <Image src={"/vote/glass/Video.png"} width={430} height={932} alt="image" className="w-full aspect-[9/19.5] h-auto rounded-in" />
